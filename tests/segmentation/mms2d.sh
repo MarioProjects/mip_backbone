b4f6933b5f36a1730fddf03299deb197befbced5
MarioProjects <maparla@inf.upv.es>
2020-12-04 11:43:04 +0100

    fix, val augmentation for val dataset!

--- tests/segmentation/mms2d.sh
+++ tests/segmentation/mms2d.sh
@@ -77,7 +77,7 @@ python3 -u train.py --gpu $gpu --dataset $dataset --model_name $model --img_size
 --scheduler $scheduler --learning_rate $lr --swa_lr $swa_lr --optimizer $optimizer --criterion $criterion \
 --normalization $normalization --weights_criterion "$weights_criterion" --data_augmentation $data_augmentation \
 --output_dir "$output_dir" --metrics iou dice --problem_type $problem_type --mask_reshape_method $mask_reshape_method \
---scheduler_steps 45 65 --generated_overlays $generated_overlays --add_depth
+--scheduler_steps 70 100 --generated_overlays $generated_overlays --add_depth
 
 #model_checkpoint="$output_dir/model_${model_name}_${epochs-swa_start}epochs_swalr${swa_lr}.pt"
 #python3 -u evaluate.py --gpu $gpu --dataset $dataset --model_name $model --img_size $img_size --crop_size $crop_size \
